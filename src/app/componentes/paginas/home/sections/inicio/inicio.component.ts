657c25c0defa486d52cda3d5e6a1ca7b7e57d48f
Marcos Paulo Silva <silvampsmarcospaulo@gmail.com>
2025-06-02 14:18:39 -0300

    finalizacao tela sobre

--- src/app/componentes/paginas/home/sections/inicio/inicio.component.ts
+++ src/app/componentes/paginas/home/sections/inicio/inicio.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-inicio',
@@ -11,4 +12,30 @@ import { TranslateModule } from '@ngx-translate/core';
 
 export class InicioComponent {
 
+  deveRetrair = true;
+  isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  ngOnInit(): void {
+    if (this.isBrowser) {
+      window.addEventListener('scroll', this.handleScroll);
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.isBrowser) {
+      window.removeEventListener('scroll', this.handleScroll);
+    }
+  }
+
+  handleScroll = (): void => {
+    this.deveRetrair = window.scrollY > 100;
+  };
+
+  AoClicarAbrirMenuRedesSociais() {
+    this.deveRetrair = !this.deveRetrair;
+  }
 }
